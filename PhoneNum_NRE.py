fbbe08c05cfcb406a7f6eba94a5fba842ef8a5e4
Justin DeOliveira <Justin DeOliveira>
2016-11-29 21:36:54 -0500

    First commit of PhoneNum_NRE.py

---
+++ PhoneNum_NRE.py
@@ -0,0 +1,43 @@
+
+# Finding patterns of text without regular expressions
+
+def isPhoneNumber(text):
+	if len (text) !=12:
+		return False
+		
+	for i in range(0,3):
+		if not text[i].isdecimal():
+			return False
+	if text[3] != '-':
+		return False
+	
+	for i in range(4,7):
+		if not text[i].isdecimal():
+			return False
+	if text[7] != '-':
+		return False
+		
+	for i in range(8,12):
+		if not text[i].isdecimal():
+			return False
+	
+	return True
+	
+print('444-555-4242 is a phone number: ', end='')
+print(isPhoneNumber('444-555-4242'))
+print('Moshi Moshi is a phone number: ', end = '')
+print(isPhoneNumber('Moshi moshi'))
+print()
+
+
+def PhoneNumberSearch(message):
+	for i in range (len(message)):
+		chunk = message[i: i+12]
+		if isPhoneNumber(chunk):
+			print('Phone number found: ' + chunk)
+
+	print('Done')	
+	
+	
+message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office'
+PhoneNumberSearch(message)	
